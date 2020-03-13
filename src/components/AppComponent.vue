3c25f72aa8b1e875f7a0e99db86cf1f883e7cb3d
Andrius <andrius.svylas@also.com>
2020-03-13 14:00:10 +0200

    webpack on save of js and vue files

--- src/components/AppComponent.vue
+++ src/components/AppComponent.vue
@@ -1,6 +1,22 @@
 <template>
-  <h1>Hello from the AppComponentawdaadawdawawawdwddwa</h1>
+  <div class="col-md6">
+    <h1>Hello from the AppComponentawdaadawdawawawdwddwa</h1>
+    <button v-on:click="count++">{{count}}</button>
+    <div class="well">
+      <p>{{greeting}}</p>
+    </div>
+  </div>
 </template>
+
 <script>
-export default {};
+export default {
+  data: function() {
+    return {
+      greeting: "Hello World",
+      count: 0
+    };
+  }
+};
 </script>
+
+
